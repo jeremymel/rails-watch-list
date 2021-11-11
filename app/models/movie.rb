a95063dd927ad87ca4a6ea9fb52104737f5e99da
Jeremy Melieres <melieresjeremy@gmail.com>
2021-11-11 14:53:34 +0100

    ad seed

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+  validates :title, uniqueness: true
+  validates :title, :overview, presence: true
+  has_many :bookmarks
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+
+movie_serialized = URI.open(url).read
+movie = JSON.parse(movie_serialized)
+
+movie["results"].each do |film|
+  title = film["title"]
+  overview = film["overview"]
+  poster_url = "https://image.tmdb.org/t/p/w500#{film["poster_path"]}"
+  rating = film["vote_average"]
+  Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
+end
